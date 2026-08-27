d72fda31523306e1120770474395bde238e009ed
LuisPedroMathias <luis_mathias@estudante.sesisenai.ogr.br>
2026-08-27 12:37:03 -0300

    ADD - conexão

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciador de Petshop</title>
+    <title>LPfood</title>
 </head>
 
 <body>
-    <h2>Pet Show!</h2>
+    <h2>LPfood</h2>
 
-    <button type="button" onclick="window.location.href='public/clientes/add_cliente.php'">Cadastrar Cliente</button>
-    <button type="button" onclick="window.location.href='public/pets/add_pets.php'">Cadastrar Pets</button>
+    <button type="button" onclick="window.location.href='public/clientes/cadastrar_cliente.php'">Cadastrar Cliente</button>
+    <button type="button" onclick="window.location.href='public/restaurantes/cadastrar_restaurante.php'">Cadastrar Restaurante</button>
+    <button type="button" onclick="window.location.href='public/restaurantes/cadastrar_pedido.php'">Cadastrar Pedido</button>
 
     <br>
     <h2>Lista de Clientes</h2>
@@ -45,31 +46,29 @@
         ?>
     </table>
 
-    <h2>Lista de Pets</h2>
+    <h2>Lista de Restaurantes</h2>
     <table>
         <th>ID</th>
         <th>Nome</th>
-        <th>Espécie</th>
-        <th>Raça</th>
-        <th>Idade</th>
-        <th>ID do Cliente</th>
+        <th>Categoria</th>
+        <th>Endereço</th>
+        <th>Telefone</th>
         <th>Ações</th>
         <?php
-        $sql = "SELECT * FROM pets";
-        $pets = $conn->query($sql);
-        while ($pet = $pets->fetch_assoc()) {
+        $sql = "SELECT * FROM restaurantes";
+        $restaurantes = $conn->query($sql);
+        while ($restaurante = $restaurantes->fetch_assoc()) {
         ?>
 
             <tr>
-                <td><?php echo $pet['id']; ?></td>
-                <td><?php echo $pet['nome']; ?></td>
-                <td><?php echo $pet['especie']; ?></td>
-                <td><?php echo $pet['raca']; ?></td>
-                <td><?php echo $pet['idade']; ?></td>
-                <td><?php echo $pet['cliente_id']; ?></td>
+                <td><?php echo $restaurante['id_restaurante']; ?></td>
+                <td><?php echo $restaurante['nome']; ?></td>
+                <td><?php echo $restaurante['categoria']; ?></td>
+                <td><?php echo $restaurante['endereco']; ?></td>
+                <td><?php echo $restaurante['telefone']; ?></td>
                 <td>
-                    <button type="button" onclick="window.location.href='public/pets/edit_pets.php?id=<?php echo $pet['id']; ?>'">Editar</button>
-                    <button type="button" onclick="if (confirm('Tem certeza que deseja excluir este pet?')) { window.location.href='public/pets/delete_pets.php?id=<?php echo $pet['id']; ?>'; }">Excluir</button>
+                    <button type="button" onclick="window.location.href='public/restaurantes/edit_restaurante.php?id=<?php echo $restaurante['id_restaurante']; ?>'">Editar</button>
+                    <button type="button" onclick="if (confirm('Tem certeza que deseja excluir este restaurante?')) { window.location.href='public/restaurantes/delete_restaurante.php?id=<?php echo $restaurante['id_restaurante']; ?>'; }">Excluir</button>
                 </td>
             </tr>
 
